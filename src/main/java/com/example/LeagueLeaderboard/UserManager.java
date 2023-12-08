491f6e88f862de3064a23dce1f6109525d2f07e0
Oroshan Hadzjaev <72888510+OroshanH@users.noreply.github.com>
2023-12-08 15:37:34 +0100

    test

--- src/main/java/com/example/LeagueLeaderboard/UserManager.java
+++ src/main/java/com/example/LeagueLeaderboard/UserManager.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class UserManager {
     public static List<User> createUsers() {
         List<User> users = new ArrayList<>();
-        String[] summonerKeys = {"nucpfTk-8jOxqEZxpRKmZajIaxB0L6hfkYG-1PtXqt2kz8Y", "1dKoepWaQD24YOrTzGJBLyA6jcPXZykKbwwp-jEn7VNqJ7g", "rwRsKG_oYqa46cxzY_iEduqEHTpcuLWKkxk2UNSxkGZr-M43", "o8ZKeiXIP9--fMl5XhuKUcL_HlIJwSMiAWyD3pD9FoCagLM", "JQHrTrMz52CSWDCXBqJcPjfEnG39trDmosJBi2SbxzOY2HY", "zvFluL5NMOtCIQ_e5OAdjntnMlc4AO2gFmaR0z8oWzmEWFw", "mMpGbfiC8L4GB2GaP4jkP7r8s8OYsMMYtRjY9EPP1Vif8w8", "i1bysGNy6oTa34mt2yJpLOq1nRmQmgh997VNbGJ3qDtwYRE", "BohVl15AUB2RMv8qXqLl9nqcgZgD825uUdI9r3ySG_lDYFkkEbtzvnIiqg", "Wdid_3BO3DYbJeEI36Xh1r4o5zc9j-Tauj9kGDTUQCzGA_P3","uHrv44LJ-fIYPh-p0hyh-16JS_cHH3a8WDjVZ5r2qZebMAQ"};
+        String[] summonerKeys = {"TELZM7qrTB8awzxtHfXu8p8goCFhHeDRTR-IT_77yxLgz5E", "1dKoepWaQD24YOrTzGJBLyA6jcPXZykKbwwp-jEn7VNqJ7g", "rwRsKG_oYqa46cxzY_iEduqEHTpcuLWKkxk2UNSxkGZr-M43", "o8ZKeiXIP9--fMl5XhuKUcL_HlIJwSMiAWyD3pD9FoCagLM", "JQHrTrMz52CSWDCXBqJcPjfEnG39trDmosJBi2SbxzOY2HY", "zvFluL5NMOtCIQ_e5OAdjntnMlc4AO2gFmaR0z8oWzmEWFw", "mMpGbfiC8L4GB2GaP4jkP7r8s8OYsMMYtRjY9EPP1Vif8w8", "i1bysGNy6oTa34mt2yJpLOq1nRmQmgh997VNbGJ3qDtwYRE", "BohVl15AUB2RMv8qXqLl9nqcgZgD825uUdI9r3ySG_lDYFkkEbtzvnIiqg", "Wdid_3BO3DYbJeEI36Xh1r4o5zc9j-Tauj9kGDTUQCzGA_P3","uHrv44LJ-fIYPh-p0hyh-16JS_cHH3a8WDjVZ5r2qZebMAQ"};
         String[] summonerNames = {"Oro", "Ost", "Sam", "Len", "Larris", "Rasmus", "Ole", "Leon","Birk","Ibra","Peter"};
         String[] profilePictureUrls = {
                 "/bilder/oro.png",
